978622f5b1279b2d30ac3888e9b1f6cdd7daa7c8
papillonyi <5414yi>
2021-06-06 00:00:35 +0800

    init aliyun client and logger

--- update_aliyun_white_list.py
+++ update_aliyun_white_list.py
@@ -1,11 +1,26 @@
 import argh
 import requests
 import ipaddress
+import logging
+from aliyunsdkcore.client import AcsClient
 
 QUERY_IP_API = "http://members.3322.org/dyndns/getip"
+logger = logging.getLogger("update aliyun white list")
+logger.setLevel(logging.DEBUG)
+log_handler = logging.StreamHandler()
+log_handler.setLevel(logging.INFO)
+formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+log_handler.setFormatter(formatter)
+logger.addHandler(log_handler)
 
 
-def run():
+def get_aliyun_client(access_key_id, access_key_secret, region_id):
+    client = AcsClient(access_key_id, access_key_secret, region_id)
+    return client
+
+
+def run(access_key_id, access_key_secret, region_id="cn-shanghai"):
+    aliyun_client = get_aliyun_client(access_key_id, access_key_secret, region_id)
     local_ip = get_local_ip()
     pass
 
@@ -13,11 +28,9 @@ def run():
 def get_local_ip():
     response = requests.get(QUERY_IP_API)
     assert response.status_code == 200
-    print(response.content)
-    ip = ipaddress.ip_address(response.content)
-    print(ip)
+    ip = ipaddress.ip_address(response.content.decode().replace("\n", ""))
+    logger.info(f"Get local ip {ip}")
     return ip
-
 
 
 if __name__ == "__main__":
