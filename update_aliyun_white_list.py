25c32fd43a541ee599da16775c43503fcb18ec4a
papillonyi <5414yi>
2021-06-06 02:44:47 +0800

    finished

--- update_aliyun_white_list.py
+++ update_aliyun_white_list.py
@@ -1,8 +1,11 @@
+import time
+
 import argh
 import requests
 import ipaddress
 import logging
 import json
+import random
 from aliyunsdkcore.client import AcsClient
 from aliyunsdkecs.request.v20140526.DescribeSecurityGroupAttributeRequest import (
     DescribeSecurityGroupAttributeRequest,
@@ -11,13 +14,17 @@ from aliyunsdkecs.request.v20140526.AuthorizeSecurityGroupRequest import (
     AuthorizeSecurityGroupRequest,
 )
 
+from aliyunsdkecs.request.v20140526.RevokeSecurityGroupRequest import (
+    RevokeSecurityGroupRequest,
+)
+
+QUERY_IP_API = "http://members.3322.org/dyndns/getip"
+
 logging.basicConfig(
     level=logging.INFO,
     format="%(asctime)s %(filename)s[line:%(lineno)d] %(levelname)s %(message)s",
     datefmt="%a, %d %b %Y %H:%M:%S",
 )
-
-QUERY_IP_API = "http://members.3322.org/dyndns/getip"
 
 
 def get_aliyun_client(access_key_id, access_key_secret, region_id):
@@ -32,8 +39,20 @@ def get_security_group(security_group_id, send_request_function):
     return send_request_function(request)
 
 
-def revoke_security_group_rule():
-    pass
+def rm_security_group_rule(home_permission, security_group_id, send_request_function):
+    request = RevokeSecurityGroupRequest()
+    request.set_Policy(home_permission["Policy"])
+    request.set_Description(home_permission["Description"])
+    request.set_Priority(home_permission["Priority"])
+    request.set_NicType(home_permission["NicType"])
+    request.set_PortRange(home_permission["PortRange"])
+    request.set_SourceCidrIp(home_permission["SourceCidrIp"])
+    request.set_IpProtocol(home_permission["IpProtocol"])
+    request.set_SecurityGroupId(security_group_id)
+    logging.info(
+        f"rm ip {home_permission['SourceCidrIp']} permission from security group {security_group_id}"
+    )
+    return send_request_function(request)
 
 
 def add_security_group_rule(
@@ -49,7 +68,7 @@ def add_security_group_rule(
     request.set_IpProtocol(home_permission["IpProtocol"])
     request.set_SecurityGroupId(security_group_id)
     logging.info(
-        f"'add local ip {local_ip} permission to security group {security_group_id}"
+        f"add local ip {local_ip} permission to security group {security_group_id}"
     )
     return send_request_function(request)
 
@@ -84,24 +103,33 @@ def get_local_ip():
 def run(access_key_id, access_key_secret, security_group_id, region_id="cn-shanghai"):
     aliyun_client = get_aliyun_client(access_key_id, access_key_secret, region_id)
     send_request_function = get_send_request_function(aliyun_client)
-    local_ip = str(get_local_ip())
-    local_ip = "1.1.1.1"
-    permissions = get_security_group(security_group_id, send_request_function)[
-        "Permissions"
-    ]["Permission"]
-    home_permission = next(filter(lambda x: x["Description"] == "home", permissions))
 
-    if local_ip == home_permission["SourceCidrIp"]:
-        logging.info(
-            f"local ip {local_ip} equal to source cider ip {home_permission['SourceCidrIp']}"
+    while True:
+        local_ip = str(get_local_ip())
+        permissions = get_security_group(security_group_id, send_request_function)[
+            "Permissions"
+        ]["Permission"]
+        home_permission = next(
+            filter(lambda x: x["Description"] == "home", permissions)
         )
-    else:
-        logging.info(
-            f"local ip {local_ip} not equal to source cider ip {home_permission['SourceCidrIp']}"
-        )
-        add_security_group_rule(
-            home_permission, local_ip, security_group_id, send_request_function
-        )
+
+        if local_ip == home_permission["SourceCidrIp"]:
+            logging.info(
+                f"local ip {local_ip} equal to source cider ip {home_permission['SourceCidrIp']}"
+            )
+        else:
+            logging.info(
+                f"local ip {local_ip} not equal to source cider ip {home_permission['SourceCidrIp']}"
+            )
+            rm_security_group_rule(
+                home_permission, security_group_id, send_request_function
+            )
+            add_security_group_rule(
+                home_permission, local_ip, security_group_id, send_request_function
+            )
+        sleep_time = random.randint(60, 60 * 10)
+        logging.info(f"sleep {sleep_time} seconds")
+        time.sleep(sleep_time)
 
 
 if __name__ == "__main__":
